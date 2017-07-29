cb6795dff42003572c52d41553cd38add8cf01bb
RussellMurad <shurjoshontan1995@gmail.com>
2017-07-29 23:44:40 +0600

    commit

--- Problem 6/Solution/Solution.cpp
+++ Problem 6/Solution/Solution.cpp
@@ -1,9 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int Recursion(int N)
+{
+    if(N < 2)
+        return 1;
+
+    return N * Recursion(N-1);
+}
+
 int main()
 {
-
+    int N;
+    cin >> N;
+    cout << "Recursion is " << Recursion(N) << endl;
     return 0;
 }
 
