f471a3640a692d77efd816b018f8bbd92ffe84a7
RussellMurad <shurjoshontan1995@gmail.com>
2017-08-01 00:27:22 +0600

    commit

--- Problem 7/Solution/Solution.cpp
+++ Problem 7/Solution/Solution.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int Recursion(int N)
+{
+    if(N < 3)
+        return 1;
+
+    return Recursion(N-1) + Recursion(N-2);
+}
+
 int main()
 {
+    int N,Result;
+    cin >> N;
+    Result = Recursion(N);
+    cout << Result;
 
     return 0;
 }
-
